8db71849a30655513cd96a6961460b43c626f625
Samuel <smarcosp@hotmail.com>
2021-09-19 18:24:00 -0300

    Piloto1

---
+++ commit01/agenda/desafio/carro/src/Modelo/Piloto.java
@@ -0,0 +1,21 @@
+package Modelo;
+
+public class Piloto extends Pessoa{
+    
+  private int matricula;
+  private String cadrasto;
+    
+     public int getMatricula() {
+       return matricula;
+     }
+     public void setMatricula(int matricula) {
+       this.matricula = matricula;
+     }
+     public String getCadrasto() {
+       return cadrasto;
+     }
+     public void setCadrasto(String cadrasto) {
+     this.cadrasto = cadrasto;
+     }    
+    
+}
